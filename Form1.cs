80b1fad6d15ce6054c70e7dd879b681f87d079ee
syouzuiken <laybaekzhang@gmail.com>
2022-06-23 13:38:45 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0623
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int vx=-10;
+        int vy=-10;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -10;
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -10;
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("章瑞謙");
+            timer1.Enabled = false;
+        }
+    }
+}
